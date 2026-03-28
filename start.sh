162b375a424f90af4d5e7ad882e566f8e6b5aa66
Benjamin Werb <55675635+Benwerb@users.noreply.github.com>
2026-03-28 13:16:22 -0700

    direct to the model path

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ source /home/ben/yolo_object/bin/activate
 export FLASK_SECRET_KEY="change-me"
 export GARDEFENDER_PASSWORD="change-me"
 export GARDEFENDER_MODEL="/home/ben/Desktop/Squirrel Cannon/yolo11n_ncnn_model"
+# To use a model in the repo instead: export GARDEFENDER_MODEL="/home/ben/Gardefender3000/yolo11n_ncnn_model"
 
 cd /home/ben/Gardefender3000
 python wsgi.py
