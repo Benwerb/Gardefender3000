eb93a256c645e04e92d5554ee1f2fd34789e9a4f
Benjamin Werb <55675635+Benwerb@users.noreply.github.com>
2026-03-28 12:02:21 -0700

    plumbing

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ source /home/ben/yolo_object/bin/activate
 
 export FLASK_SECRET_KEY="change-me"
 export GARDEFENDER_PASSWORD="change-me"
-export GARDEFENDER_DETECTIONS="/home/ben/Desktop/Squirrel Cannon/detections"
 export GARDEFENDER_MODEL="/home/ben/Desktop/Squirrel Cannon/yolo11n_ncnn_model"
 
 cd /home/ben/Gardefender3000
